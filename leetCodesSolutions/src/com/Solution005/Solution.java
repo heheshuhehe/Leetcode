3c01c061178f23da6e2bfa6e07021fac83a30cfa
shuchengge <xiaoyudian316@gmail.com>
2020-12-13 17:25:56 +0800

    excute succeeded for 005, wwaiting to commit and test it in leetcode

--- leetCodesSolutions/src/com/Solution005/Solution.java
+++ leetCodesSolutions/src/com/Solution005/Solution.java
@@ -5,12 +5,33 @@ import java.util.Map;
 
 public class Solution {
     public String longestPalindrome(String s) {
-        String result ="";
-        int maxLength =0;
+        String result = "";
+        int maxLength = 1;
+        int currentLength = 0;
+        int maxIndex = 0;
+        int sIndex= 0;
+        boolean isPalindromeFlg=true;
         Map <String, Integer> currentMap= new HashMap<>();
-        for (int i=0;i<s.length();i++){
-            currentMap.put(String.valueOf(s.charAt(i)),i);
+        for (int mIndex=0;mIndex<s.length();mIndex++){
+            sIndex= mIndex;
+            currentLength=0;
+            for (int mTailIndex = s.length()-1;mTailIndex>=sIndex;mTailIndex--){
+                if (s.charAt(sIndex)==s.charAt(mTailIndex)){
+                    currentLength+=(sIndex==mTailIndex?1:2);
+                    sIndex++;
+                }else {
+                    currentLength=0;
+                    sIndex=mIndex;
+                }
+                System.out.println("currentLength is "+currentLength + ", mIndex is "+mIndex+", sIndex is "+(sIndex-1) + ", mTailIndex is "+mTailIndex);
+            }
+            if ( currentLength > maxLength)  {
+                maxLength = currentLength;
+                maxIndex = mIndex;
+            }
         }
+        System.out.println("maxIndex is " + maxIndex + ", maxLength is "+maxLength);
+        result = s.substring(maxIndex,maxIndex+maxLength);
         return result;
     }
 }
